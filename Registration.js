f06c0587d3e9e0b5e854c738656350d901601d95
Rathore-Aditya <42271261+Rathore-Aditya@users.noreply.github.com>
2019-04-11 07:07:34 +0530

    Registration.js

--- Registration.js
+++ Registration.js
@@ -1,7 +1,8 @@
- 
+
 import React, { Component } from 'react';
 import Modal from 'react-awesome-modal';
 import indexf from './index';
+import axios from 'axios'; 
 import cross from './img/cross.jpg'
 import {BrowserRouter as Router,Link} from 'react-router-dom';
 import Route from 'react-router-dom/Route';
@@ -12,12 +13,55 @@ export default class Registration extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            visible : true
+            visible : true,
+            uname: '',
+            pass: '',
+            email: '',
+            loading: false,
+            message: ''
         }
     }
-
+    dataChange(event)
+    {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
     
-
+    postData(event)
+    {
+        event.preventDefault();
+        const name=this.state.uname;
+        const pass=this.state.pass;
+        const email=this.state.email;
+        this.setState({loading: true})
+        const regisData={
+            name,
+            pass,
+            email
+        }
+        axios.post('/t/lyyg4-1554869078/post',regisData).then(response=>{
+            this.setState({
+                loading: false,
+                message: response.regisData
+            })
+        }).catch(err=>{
+            console.log(err);
+            this.setState({loading: false})
+        })
+    }
+    loadorshow()
+    {
+        if(this.state.loading)
+        {
+            return alert('Registration Succesful!!!');
+            
+        }
+        else
+        {
+            return <p>{this.state.message}</p>
+        }
+    }
     closeModal() {
         this.setState({
             visible : false
@@ -34,20 +78,18 @@ export default class Registration extends Component {
                     effect="fadeInUp"
                     
                  >
-                    <div className="RegistrationContainer">
+                    <form onSubmit={this.postData.bind(this)}>
                         <h1>Registration</h1>
                         <Link to="/login" onClick={() => this.closeModal()}><img src={cross} className="Logo"/></Link>
                         <Route path="/login" component={login}/>
                         <div className="Rbox">
-                         <input type="text" name="uname" placeholder="UserName"/>
-                         <input type="password" name="pass" placeholder="Password"/><br/>
-                         <input type="text" name="email" placeholder="Email"/><br/>
+                         <input type="text" name="uname" placeholder="UserName" value={this.state.uname}  onChange={this.dataChange.bind(this)} required="true" autoComplete="off"/>
+                         <input type="password" name="pass" placeholder="Password" value={this.state.pass} onChange={this.dataChange.bind(this)} required="true"/><br/>
+                         <input type="text" name="email" placeholder="Email" value={this.state.email} onChange={this.dataChange.bind(this)} required="true" autoComplete="off"/><br/>
                          <button type="submit" className="Rbutton1">Submit</button>
-                         
-                         
-                        </div>   
-                    </div>
-                     
+                         </div>   
+                    </form>
+                     {this.loadorshow()}
                 </Modal>
             </section>
         );
