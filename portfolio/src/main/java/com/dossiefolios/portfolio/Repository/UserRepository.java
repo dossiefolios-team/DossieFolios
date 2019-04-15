c59b61ccc201c880518b057e5bdc547b2dfd5072
adi1862 <33635849+adi1862@users.noreply.github.com>
2019-04-15 22:27:53 +0530

    UserRepository Updated

--- portfolio/src/main/java/com/dossiefolios/portfolio/Repository/UserRepository.java
+++ portfolio/src/main/java/com/dossiefolios/portfolio/Repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.dossiefolios.portfolio.Repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -10,9 +11,10 @@ import com.dossiefolios.portfolio.domain.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long>{
-   List<User> fetchByUserid(@Param("id") Long userid);
+	@Query(name ="User.fetchByUsername")
+   List<User> fetchByUsername(@Param("username") String username);
 
-User findOne(String username);
+//User findOne(String username);
 
-User findOne(Long userid);
+//User findOne(Long userid);
 }
