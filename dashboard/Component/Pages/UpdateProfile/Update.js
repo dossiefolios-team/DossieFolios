6bcbe2fc2f661f4fc4a829409afb901497807150
Muskan Gupta <32198462+muskangupta423@users.noreply.github.com>
2019-04-15 22:28:58 +0530

    Update Update.js

--- dashboard/Component/Pages/UpdateProfile/Update.js
+++ dashboard/Component/Pages/UpdateProfile/Update.js
@@ -1,17 +1,8 @@
 import React from 'react'
 import ReactCardFlip from 'react-card-flip';
-import arya from './arya.png';
+ 
 import './Update.css';
-/*const Update=()=>{
-    return(
-        <div className="containerUpdate">
-             Update
-        </div>    
-    );
-}
-
-export default Update;*/
-
+ 
 export default class Update extends React.Component {
     constructor() {
       super();
@@ -29,25 +20,8 @@ export default class Update extends React.Component {
     render() {
       return (
         <div className="containerUpdate">
-        
-        <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="vertical">
-          <div key="front" className="front">
-             
-          <img className="imagearya" src={arya}/>
-          <p>Aditya Arya</p>
-            <button onClick={this.handleClick}>More Info</button>
-          </div>
-   
-          <div key="back" className="back">
-            <p>B.Tech C.Se(2016-2020)</p><br/>
-            <p>GLA University,Mathura(U.P)</p>
-            <i className="fa fa-facebook"></i>
-            <i className="fa fa-twitter"></i>
-            <i className="fa fa-linkedin"></i>
-            <i className="fa fa-github"></i>
-            <button onClick={this.handleClick}>Back</button>
-          </div>
-        </ReactCardFlip></div> 
+          <p>/this is update section</p>
+         </div> 
       )
     }
   }
